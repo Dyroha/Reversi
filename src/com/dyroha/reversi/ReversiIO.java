2b33e1391dc3c48f064ac4e4c30c8721e4c662cf
Dylan Hamilton <dylahami@gmail.com>
2021-04-23 13:57:19 +0100

    added documentation fro ReversiIO class

--- src/com/dyroha/reversi/ReversiIO.java
+++ src/com/dyroha/reversi/ReversiIO.java
@@ -5,16 +5,34 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
+/**
+ * File Save and load for a GameSession
+ * 
+ * @version 23/04/2021
+ * @author Dylan Hamilton
+ */
 public class ReversiIO {
-
+	// private constructor to hide the public super one
 	private ReversiIO() {}
 
+	/**
+	 * Save a GameSession to a given file
+	 * @param gameFile the file for which to save the session to
+	 * @param session the session to save to file
+	 * @throws IOException if the file cannot be found
+	 */
 	public static void saveSession(File gameFile, GameSession session) throws IOException {
 		try (FileWriter fw = new FileWriter(gameFile)) {
 			fw.write(session.getFileFormattedString());
 		}
 	}
 
+	/**
+	 * Load a GameSession from a file
+	 * @param gameFile the file to load the game from
+	 * @return the loaded GameSession object
+	 * @throws IOException if the file cannot be found or read propperly, or if the GameSession couln't load the pieces
+	 */
 	public static GameSession loadSession(File gameFile) throws IOException {
 		try (Scanner sc = new Scanner(gameFile).useDelimiter(";")) {
 			String blackName = sc.next();
